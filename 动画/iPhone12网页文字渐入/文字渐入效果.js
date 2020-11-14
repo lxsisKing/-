df905ae95df8bf3594bf4edc76987b7efd986769
Mr. Liu <1049301771@qq.com>
2020-11-15 00:49:19 +0800

    给“iPhone12网页文字渐入”添加注释

--- 动画/iPhone12网页文字渐入/文字渐入效果.js
+++ 动画/iPhone12网页文字渐入/文字渐入效果.js
@@ -1,5 +1,8 @@
 document.addEventListener('scroll', (e) => {
     const h1 = document.querySelector('#text')
+    // document.documentElement.scrollTop获取滚动条的位置
+    // document.documentElement.scrollHeight获取浏览器所有内容高度
+    // document.documentElement.clientHeight获取浏览器可视内容高度
     let scrolled = document.documentElement.scrollTop /
         (document.documentElement.scrollHeight -
             document.documentElement.clientHeight)
